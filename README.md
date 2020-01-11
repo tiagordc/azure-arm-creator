7fce11da4668df7652a27cb8e9dc59d496de61d1
Tiago Costa <tiagordc@users.noreply.github.com>
2020-01-11 09:24:21 +0000

    ipad test

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Allow your users to create and manage azure resources (based on ARM templates) without any kind of cloud knowledge.
 
+Developed with minimum possible assets to work on an iPad with Pythonista 3.
+
 Azure, ARM, Python, Flask, Framework7
 
 ## Folder structure
@@ -12,4 +14,5 @@ Azure, ARM, Python, Flask, Framework7
         * template.json - ARM template
 
 ## Configuration
+
 
--- app.py
+++ app.py
@@ -7,35 +7,36 @@ template_folder = 'azure'
 
 @app.route('/static/<path:path>')
 def static_files(path):
-  return send_from_directory('static', path)
+	return send_from_directory('static', path)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+	return render_template('index.html')
 
 @app.route('/templates')
 def templates():
-  result = []
-  for root, dirs, files in os.walk(template_folder):
-    if root.startswith(template_folder + '\\') and "template.json" in files:
-      template_image = None
-      for filepath in files:
-        if filepath.endswith(".png") or filepath.endswith(".jpg"):
-          with Image.open(root + '\\' + filepath) as img:
-            width, height = img.size
-            if width == height and width == 256:
-              template_image = filepath
-              break
-      if template_image == None:
-        break
-      item_name, img_ext = os.path.splitext(template_image)
-      item = { "name": item_name, "path": root[len(template_folder)+1:], "image": template_image }
-      result.append(item)
-  return jsonify(result)
+	result = []
+	for root, dirs, files in os.walk(template_folder):
+		if root.startswith(template_folder + '\\') and "template.json" in files:
+			template_image = None
+			for filepath in files:
+				if filepath.endswith(".png") or filepath.endswith(".jpg"):
+					with Image.open(root + '\\' + filepath) as img:
+						width, height = img.size
+						if width == height and width == 256:
+							template_image = filepath
+							break
+			if template_image == None:
+				break
+			item_name, img_ext = os.path.splitext(template_image)
+			item = { "name": item_name, "path": root[len(template_folder)+1:], "image": template_image }
+			result.append(item)
+	return jsonify(result)
 
 @app.route('/templates/<path:path>')
 def template(path):
-  return send_from_directory(template_folder, path)
+	return send_from_directory(template_folder, path)
 
 if __name__ == '__main__':
-  app.run()
+	app.run()
+	
