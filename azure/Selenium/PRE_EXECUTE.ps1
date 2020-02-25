e3915164205c135bceee476eea75e162c1a8b2e7
Tiago Costa <tiagorc@gmail.com>
2020-02-25 16:45:21 +0000

    removed selinium

--- azure/Selenium/PRE_EXECUTE.ps1
+++
@@ -1,6 +0,0 @@
-
-param ([int]$logLevel = 0, [string]$serviceUrl, [string]$resourceGroup, [string]$vmName, [string]$zipDownload, [string]$zipFolder, [string]$packages, [string]$userName, [string]$userPass)
-
-Write-Output "PRE EXECUTE - Disable Firewall"
-Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
-Get-NetFirewallProfile
