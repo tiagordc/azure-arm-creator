068e323ad014c4031aebf7409156a8681e28a34d
Tiago Costa <tiagorc@gmail.com>
2020-02-26 09:35:50 +0000

    fix

--- azure/Crawler/Orchestrator.sh
+++ azure/Crawler/Orchestrator.sh
@@ -1,5 +1,11 @@
+
 sudo apt update
 
-sudo apt install -y build-essential curl file git linuxbrew-wrapper
+sudo apt install -y build-essential curl file git linuxbrew-wrapper vsftpd
 
 brew install redis
+
+sudo sed -r -i "s/(anonymous_enable)=(.*)/\1=YES/g" /etc/vsftpd.conf
+
+sudo systemctl start vsftpd
+sudo systemctl enable vsftpd
--- azure/Kafka/Zoo.sh
+++ azure/Kafka/Zoo.sh
@@ -10,16 +10,16 @@ IP_PREFIX=$6
 yum -y install java-1.8.0-openjdk
 yum -y install wget
 
-mkdir -p /var/lib/zookeeper
-cd /var/lib/zookeeper
+# mkdir -p /var/lib/zookeeper
+# cd /var/lib/zookeeper
 
-wget https://archive.apache.org/dist/kafka/2.0.0/kafka_2.12-2.0.0.tgz
-tar --strip-components=1 -xzf kafka_2.12-2.0.0.tgz
-rm -f kafka_2.12-2.0.0.tgz 
+# wget https://archive.apache.org/dist/kafka/2.0.0/kafka_2.12-2.0.0.tgz
+# tar --strip-components=1 -xzf kafka_2.12-2.0.0.tgz
+# rm -f kafka_2.12-2.0.0.tgz 
 
-echo 'export PATH=$PATH:/var/lib/zookeeper/bin' >>~/.bash_profile
-echo "/var/lib/zookeeper/bin/zookeeper-server-start.sh /var/lib/zookeeper/config/zookeeper.properties> /dev/null 2>&1 &" >>/etc/rc.d/rc.local
+# echo 'export PATH=$PATH:/var/lib/zookeeper/bin' >>~/.bash_profile
+# echo "/var/lib/zookeeper/bin/zookeeper-server-start.sh /var/lib/zookeeper/config/zookeeper.properties> /dev/null 2>&1 &" >>/etc/rc.d/rc.local
 
-chmod +x /etc/rc.d/rc.local
-systemctl enable rc-local
-systemctl start rc-local
+# chmod +x /etc/rc.d/rc.local
+# systemctl enable rc-local
+# systemctl start rc-local
